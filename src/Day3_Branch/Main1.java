d14c2a6a6f18514022da234f0bcb79d480fc9a16
Batch-21 ForLearning <samadigithub@gmail.com>
2020-11-12 10:58:46 -0500

    lets observe the change

--- src/Day3_Branch/Main1.java
+++ src/Day3_Branch/Main1.java
@@ -2,7 +2,9 @@ package Day3_Branch;
 
 public class Main1 {
     public static void main(String[] args) {
-        System.out.println("Hello Git Last Day")
-        ;
+
+        // write your code here
+        System.out.println("Hello Git Last Day");
+        //B21 is too exited
     }
 }
