c72c8cd546a972c16c0d40b2de0e4a7d113617ca
Batch-21 ForLearning <samadigithub@gmail.com>
2020-11-12 11:00:37 -0500

    Second change

--- src/Day3_Branch/Main1.java
+++ src/Day3_Branch/Main1.java
@@ -6,5 +6,7 @@ public class Main1 {
         // write your code here
         System.out.println("Hello Git Last Day");
         //B21 is too exited
+        // B21 is dedicated
+        // B21 is successful testers
     }
 }
